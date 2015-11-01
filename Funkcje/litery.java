920609d09d8da3133faf94ee75c7d7804b8a7cad
marco146 <hellboy1908@o2.pl>
2015-11-01 16:49:08 +0100

    Update litery.java

--- Funkcje/litery.java
+++ Funkcje/litery.java
@@ -1,1 +1,37 @@
 
+package litery;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+public class Litery {
+
+    public static void foo(String plik)
+    {
+         
+        try{
+            RandomAccessFile raf = new RandomAccessFile("zapis.txt","rw");
+            Scanner sc = new Scanner(new File(plik));
+          
+          while(sc.hasNextInt())
+          {
+              
+               int a=sc.nextInt();
+               System.out.println(a);
+                raf.writeBytes((char) (a==0?' ':(a+ '`'))+"\r\n");
+          }
+
+            System.out.println("Sukces");
+           
+        }catch(IOException e){System.out.println(e);};
+    }
+   
+    public static void main(String[] args) {
+
+         
+        foo("c:/test/liczby.txt");
+    }
+    
+}
